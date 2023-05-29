a0c56f745372c8f7bd392fb0c438fb7af08daf57
keyamoni05866 <keya05866@gmail.com>
2023-05-30 01:21:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('boss siting')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nn0l6mi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+   const menuCollection = client.db('bistroDb').collection('menu');
+   const reviewsCollection = client.db('bistroDb').collection('reviews');
+   const cartCollection = client.db('bistroDb').collection('carts');
+
+   app.get('/menu', async(req, res) =>{
+    const result = await menuCollection.find().toArray();
+    res.send(result)
+   })
+   app.get('/reviews', async(req, res) =>{
+    const result = await reviewsCollection.find().toArray();
+    res.send(result)
+   })
+
+  //  for cart collections post 
+  app.post('/carts', async(req, res) =>{
+    const item = req.body;
+    const result = await cartCollection.insertOne(item);
+    res.send(result);
+  })
+  // for cart collections apis
+  app.get('/carts', async(req, res) =>{
+    const email = req.query.email;
+    if(!email){
+      res.send([]);
+    }
+    const query = {email : email};
+    const result = await cartCollection.find(query).toArray();
+    res.send(result)
+
+  })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () =>{
+    console.log(`Bistro boss is siting on port ${port}`)
+})
