ccfd545c2bd1097b1cf96bd5be1becebe5ec0f2a
keyamoni05866 <keya05866@gmail.com>
2023-05-30 16:35:26 +0600

    deleted operations done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -63,6 +63,13 @@ async function run() {
     const result = await cartCollection.find(query).toArray();
     res.send(result)
 
+  })
+  // cart delete
+  app.delete('/carts/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result)
   })
 
   } finally {
